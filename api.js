b5357cfbd495dc744226e69cd053321f0034155d
Rida-e-Fatima <126771470+Rida-e-Fatima@users.noreply.github.com>
2023-04-19 14:23:53 +0500

    worldtimeapi

---
+++ api.js
@@ -0,0 +1,22 @@
+// Get the element where the result will be displayed
+const resultDiv = document.getElementById("result");
+
+// Fetch the data from the API
+url = "http://worldtimeapi.org/api/ip";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // Create a string with the data to display
+    const dateString = data.datetime;
+    const timezone = data.timezone;
+    const displayString = `The current time is ${dateString} in the ${timezone} timezone.`;
+
+    // Add the result to the HTML
+    resultDiv.innerText = displayString;
+  })
+  .catch((error) => {
+    // If there's an error, display a message
+    resultDiv.innerText = "An error occurred while fetching the data.";
+  });
+
+
